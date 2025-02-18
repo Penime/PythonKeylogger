11845417597d5554cce1b4d18a1006e0c3b43834
Penime <penime1ather@proton.me>
2025-02-18 15:15:23 +0200

    added decryption file

--- decryptor.py
+++ decryptor.py
@@ -1,8 +1,8 @@
-def decrypt(data:bytes):
-    if isinstance(data, str):
-        data = data.encode('utf-8')
+def decrypt(path: str) -> bytes:
     key = b'KopLeoRos'
-    return bytes([data[i] ^ key[i % len(key)] for i in range(len(data))]).decode(errors='ignore')
+    with open(path, 'rb') as file:
+        data: bytes = file.read()
+    return bytes([data[i] ^ key[i % len(key)] for i in range(len(data))])
 
-print(decrypt('0e01133e1c1f26061c25'.encode('hex')))
-    
+
+print(decrypt('encrypted_data.txt'))
--- key_logger/encryptor.py
+++ key_logger/encryptor.py
@@ -5,10 +5,12 @@ class XorEcryptor(Encyptor):
     def __init__(self) -> None:
         self.__key = b'KopLeoRos'
 
-    def encrypt(self, data: str | dict) -> str:
+    def encrypt(self, data: str | dict) -> bytes:
         data = str(data).encode()
         return bytes([data[i] ^ self.__key[i % len(self.__key)] for i in range(len(data))])
 
+
 if __name__ == '__main__':
     x = XorEcryptor()
-    print(x.encrypt('Encryption').decode())
+    with open('encrypted_data.txt', 'wb') as file:
+        file.write(x.encrypt('Encryption'))
