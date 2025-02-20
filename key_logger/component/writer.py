21a2f5006921cef411dc68229d80652cd0f7f0b0
Penime <penime1ather@proton.me>
2025-02-20 11:23:33 +0200

    update debug

--- key_logger/component/writer.py
+++ key_logger/component/writer.py
@@ -2,24 +2,28 @@ from interfaces import Writer
 
 
 class FileWriter(Writer):
-    def __init__(self):
-        pass
+    # def __init__(self):
+    #     pass
 
     def send_data(self, data):
         pass
 
 
 class JsonWriter(Writer):
-    def __init__(self):
-        pass
+    # def __init__(self):
+    #     pass
 
     def send_data(self, data: dict):
         pass
 
 
 class NetworkWriter(Writer):
-    def __init__(self):
+    # def __init__(self):
+    #     pass
+
+    def send_data(self, data):
         pass
 
-    def send_data(self, data: str | dict):
-        pass
+
+if __name__ == '__main__':
+    FileWriter.send_data()
